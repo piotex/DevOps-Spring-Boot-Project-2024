5762235e1d433277a83c183bf4ad51b9db49e928
piotex <pkubon2@gmail.com>
2024-11-04 22:35:19 +0100

    test

--- spring-boot-simple/src/main/java/com/baeldung/starter/repository/GenericEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.starter.repository;
-
-import com.baeldung.starter.domain.GenericEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> {
-}
